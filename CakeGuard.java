69516aca34d7abc8ca3ed92d138dff87b6e688ff
Yarik1488-52 <pogoriluyyroslav@gmail.com>
2026-04-14 16:13:26 +0300

    Update CakeGuard.java

--- CakeGuard.java
+++ CakeGuard.java
@@ -1,37 +1,57 @@
-package me.yarik.cakeguard;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class CakeGuard extends JavaPlugin implements Listener {
 
-    private final Pattern filter = Pattern.compile(".*(м[.|_]*а[.|_]*т[.|_]*ь|м[.|_]*а[.|_]*м|б[.|_]*а[.|_]*т[.|_]*я|о[.|_]*т[.|_]*ч[.|_]*и[.|_]*м|m[.|_]*q|к[.|_]*у).*");
+    private final HashMap<UUID, Long> cooldowns = new HashMap<>();
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
+        String msg = e.getMessage().toLowerCase();
+        UUID uuid = p.getUniqueId();
+
+        // Ігнор для тебе
         if (p.getName().equalsIgnoreCase("Yarikznaeprava")) return;
 
-        String msg = e.getMessage().toLowerCase().replaceAll("\\s+", "");
+        // --- КУЛДАУН 3 СЕКУНДИ ---
+        if (cooldowns.containsKey(uuid)) {
+            long timeLeft = ((cooldowns.get(uuid) / 1000) + 3) - (System.currentTimeMillis() / 1000);
+            if (timeLeft > 0) {
+                e.setCancelled(true);
+                p.sendMessage(ChatColor.RED + "Зачекайте ще " + timeLeft + " сек.");
+                return;
+            }
+        }
+        cooldowns.put(uuid, System.currentTimeMillis());
 
-        if (filter.matcher(msg).matches()) {
+        // --- ПЕРЕВІРКА НА ОБРАЗИ (Рідні, Адмін, Проекти) ---
+        // Тут тільки те, за що реально треба карати
+        if (msg.matches(".*(мать|маму|батя|отчим|mamy|mamu|mq|админ лох|сервер говно|мать ебал).*")) {
             e.setCancelled(true);
             String n = p.getName();
+            
             Bukkit.getScheduler().runTask(this, () -> {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tempmute " + n + " 12h 3.1 (Образа рідних)");
-                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&c&lPUNISH &8» &fГравець &e" + n + " &fзамучений за образу рідних!"));
+                // Команда муту
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tempmute " + n + " 12h 3.1 (Образа рідних/адміністрації)");
+                
+                // Оголошення в чат
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&8&l&m---------------------------------------"));
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', " &c&lPUNISH &8» &fГравець &e" + n + " &fзамучений!"));
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', " &c&lPUNISH &8» &fПричина: &7Неадекватна поведінка (3.1)"));
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&8&l&m---------------------------------------"));
             });
         }
     }
